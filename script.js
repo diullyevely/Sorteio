3ec38625d2236488dd12426b52be40e4d62b4d7e
diullyevely <diullyevelyduartevieira31@gmail.com>
2025-02-26 13:14:09 -0300

    mudança script

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function sorteio() {
   const input1 = Math.ceil(document.querySelector(".primeiro-input").value);
   const input2 = Math.floor(document.querySelector(".segundo-input").value);
   const resultado = document.querySelector(".resultado")
+  if ( input1>=input2){
+    alert(" O primeiro valor tem que ser MENOR que o segundo")
+  }
   
   const result = Math.floor(Math.random() * (input2-input1+1)+input1)
  
